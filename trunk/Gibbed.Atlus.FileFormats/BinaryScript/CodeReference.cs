aeff4e36a23134bcac8c495e3d790f411bbc59ee
rick <rick@a8076a73-a099-df11-ba30-0030489379b8>
2011-09-13 07:33:08 +0000

    - Move over to Gibbed.IO. - Solution and project cleanup.

--- trunk/Gibbed.Atlus.FileFormats/BinaryScript/CodeReference.cs
+++ trunk/Gibbed.Atlus.FileFormats/BinaryScript/CodeReference.cs
@@ -1,9 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
 using System.IO;
-using System.Runtime.InteropServices;
-using Gibbed.Atlus.FileFormats.Script;
-using Gibbed.Helpers;
+using Gibbed.IO;
 
 namespace Gibbed.Atlus.FileFormats.BinaryScript
 {
@@ -15,7 +34,7 @@ namespace Gibbed.Atlus.FileFormats.BinaryScript
 
         public void Deserialize(Stream input, bool littleEndian)
         {
-            this.Name = input.ReadStringASCII(24, true);
+            this.Name = input.ReadString(24, true, GameEncoding.ASCII);
             this.Offset = input.ReadValueU32(littleEndian);
             this.Unknown1C = input.ReadValueU32(littleEndian);
 
